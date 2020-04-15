dfa25e603c609495ae6f1a09d5f5760473b5d387
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-04-15 23:12:23 +0900

    [Fix]ジャンケンゲーム

--- janken.rb
+++ janken.rb
@@ -11,30 +11,25 @@ def janken
 	if you == me
 		puts "あいこで　しょ"
 		next_janken = true
-
 		while next_janken == true
           next_janken = janken
         end
 
 	elsif (you == 0 && me == 1)||(you == 1 && me == 2)||(you == 2 && me == 0)
-		puts "君の勝ちだ"
+		puts "君の 勝ちだ …"
 	else
 		puts "僕の　勝ちだ　！"
     end
 end
 
-puts "======ジャンケン小僧がやって来るッ！======"
+puts "======　ジャンケン小僧がやってくる　！　======"
 
 puts "ぼくと「ジャンケン」してくれない？"
 
 num = 1
 while num <= 5
-
   puts "=== #{num}回目の勝負 ==="
-  puts "ジャーンケーン　ホイッ！"
+  puts "ジャーンケーン　ほい　！"
   janken
-
-
-
   num += 1
 end
